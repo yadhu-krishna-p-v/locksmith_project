e7be697f859150a8ebc0252332748c0fe79b7b75
yadhu.pv <yadhu.pv@admaren.com>
2025-01-30 10:50:02 +0530

    added docker file and some api view

--- locksmith_project/locksmiths/urls.py
+++ locksmith_project/locksmiths/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import LocksmithProfileListCreateView, LocksmithProfileDetailView
+from .views import approve_locksmith
 
 urlpatterns = [
     path('', LocksmithProfileListCreateView.as_view(), name='locksmith-list'),
     path('<int:pk>/', LocksmithProfileDetailView.as_view(), name='locksmith-detail'),
+    path('approve/<int:locksmith_id>/', approve_locksmith, name='approve_locksmith'),
 ]
